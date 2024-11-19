c91be84fe3c5c087eff3ce8a7ec96e953aa560f1
Ganesh <selvamganeshkumar@gmail.com>
2024-11-19 15:56:11 +0530

    thrid commit

--- GitSourceControlDemo/ContentView.swift
+++ GitSourceControlDemo/ContentView.swift
@@ -15,9 +15,6 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Swift!")
-            Button("Click me!") {
-                
-            }
         }
         .padding()
     }
