c6d0af0f578d67dc37b0a97ee2162e54b61d8c09
Ganesh <selvamganeshkumar@gmail.com>
2024-11-19 15:18:19 +0530

    Initial Commit

---
+++ GitSourceControlDemo/GitSourceControlDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlDemoApp.swift
+//  GitSourceControlDemo
+//
+//  Created by Ganesh Kumar on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
