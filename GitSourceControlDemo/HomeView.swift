6c94dfcbc96e85edf3e8d9ba0797cfcc235ea14e
Ganesh <selvamganeshkumar@gmail.com>
2024-11-19 16:03:24 +0530

    Added Home View

---
+++ GitSourceControlDemo/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitSourceControlDemo
+//
+//  Created by Ganesh Kumar on 19/11/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!!!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
